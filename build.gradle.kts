882aea92fc9bd9088c1071f11d5fa11c8bccf72f
root <root@localhost.localdomain>
2026-05-15 06:41:58 +0000

    Initial Commit - High-End Kotlin Template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    
+    // Upgraded from 2.0.0 to 2.2.21 to support LiteRT's modern dependencies
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
 }
